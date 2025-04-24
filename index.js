ec4f8563b676e46219209420c683b09ccbfe773a
Abdellatif Elfarouah <abdellatiflfr2000@gmail.com>
2025-04-24 18:02:11 +0100

    adding getComputerChoice function

---
+++ index.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    if (Math.floor(Math.random()*3) == 0) {
+        return "rock" ;
+    }else if (Math.floor(Math.random()*3) == 1) {
+     return "paper"   ;
+    }else{
+        return "scissors" ;
+    }
+}
+console.log(getComputerChoice());
